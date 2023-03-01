82cdc71f5490c8f571b802fbf6f944d78a92bcdf
Gustavo <gustavo.zucolo@hotmail.com>
2023-02-28 21:51:23 -0300

    create one user on test

--- src/main/java/br/com/cashnow/test/DBTest.java
+++ src/main/java/br/com/cashnow/test/DBTest.java
@@ -1,10 +1,36 @@
 package br.com.cashnow.test;
 
+import br.com.cashnow.domain.User;
+
+import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 public class DBTest {
 
     public static void main(String[] args) {
-        Persistence.createEntityManagerFactory("cashnow").createEntityManager();
+        EntityManager em = null;
+        try {
+            em = Persistence
+                    .createEntityManagerFactory("cashnow")
+                    .createEntityManager();
+            User user = new User(
+                    "12345678910",
+                    "gustavo",
+                    "01123456789",
+                    "gustavoteste@fiap.com.br"
+            );
+            em.merge(user);
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em != null && em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+            System.exit(0);
+        }
     }
 }
