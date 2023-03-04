41a0a653773dc4410ca41b4d0d69663e4f858889
Gustavo <gustavo.zucolo@hotmail.com>
2023-03-04 19:11:37 -0300

    interface deleted

--- src/main/java/br/com/cashnow/ITransactions.java
+++
@@ -1,8 +0,0 @@
-package br.com.cashnow;
-
-import br.com.cashnow.dao.UserDAO;
-import br.com.cashnow.domain.User;
-
-public interface ITransactions {
-    public void create(UserDAO userDAO, User user);
-}
